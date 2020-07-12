ec5821cf44f83542e924ce6a14c657938dec7fba
いずみ <izumi@Matsuo-MacBook-Air.local>
2020-07-13 00:28:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ansible-role-haproxy
+
+CentOS 7 に haproxy/keepalived を導入する ansible role です。
+
+## 設定項目
+
+以下の設定項目は上書き可能。
+
+| 項目名             | デフォルト値| 説明               |
+| ------------------ | ----------- | ------------------ |
+| haproxy_frontend_bind | *:80     | INPUTポート番号    |
+| haproxy_balance_logic | roundrobin | 振分けロジック   |
+| haproxy_backend_servers | none   | 振分け先サーバ     |
+| keepalived_check_interfale | none | チェックするNIC   |
+| keepalived_virtual_ipaddr | none | VIP                |
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,29 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_haproxy_is_installed(host):
+    package = host.package("haproxy")
+    assert package.is_installed
+    assert package.version.startswith("1.5")
+
+
+def test_haproxy_running_and_enabled(host):
+    service = host.service("haproxy")
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_haproxy_listen(host):
+    assert host.socket('tcp://0.0.0.0:80').is_listening
+
+
+def test_keepalived_is_installed(host):
+    package = host.package("keepalived")
+    assert package.is_installed
+    assert package.version.startswith("1.3")
